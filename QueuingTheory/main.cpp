e1f512d950c3f9df5a46bcf1c2ccdad2a60f81da
MrDoffing <nathanael.remstedt@outlook.com>
2020-10-09 22:06:54 +0200

    stuff done

--- QueuingTheory/main.cpp
+++ QueuingTheory/main.cpp
@@ -13,164 +13,195 @@ string replace(string source, char target, char replacer) {
 	return source;
 }
 
-struct ModelInfo {
-	int n;
-	double Ui, Rprime, Ri, X, Qi;
-	void print() {
-		cout << "n = " << n << endl;
-		cout << "Ui = " << Ui << "%" << endl;
-		cout << "Rprime[n] = " << Rprime << endl;
-		cout << "Ri = " << Ri << endl;
-		cout << "X[n] = " << X << endl;
-		cout << "Qi[n] = " << Qi << endl;
-		cout << endl;
-	}
-	ModelInfo(int _n, double _Ui, double _Rprime, double _Ri, double _X, double _Qi) {
-		n = _n;
-		Ui = _Ui;
-		Rprime = _Rprime;
-		Ri = _Ri;
-		X = _X;
-		Qi = _Qi;
+struct Node {
+	string name = "";
+	double demand = 0;
+	double Q = 0;
+	double Rprime = 0;
+	Node(double _demand = 0, string _name = "") {
+		demand = _demand;
+		name = _name;
 	}
 };
-class ClosedModel {
-public:
-	vector<double> Qi; // Queue length at node i
-	vector<double> Rprime; // Residence time at queuing node i
-	vector<double> X; //System throughput
 
-	int m = 1; // Number of queuing nodes
-	int N = 50; // Number of customers
-
-	double Di = 0.0394;//0.25 // Service demand, Rprime[1] = Di -> Residence time at first queuing node
-	double Z = 0.0;//2.0 // Think time //Big brain time
-
-public:
-	ClosedModel(){}
-	ClosedModel(double _Di, double _Z, int _N) {
-		Di = _Di;
-		Z = _Z;
-		N = _N;
-		Qi.resize(_N);
-		Rprime.resize(_N);
-		X.resize(_N);
+struct System {
+	int thinkTime = 0;
+	vector<Node> nodes;
+	double X = 0;
+	double ResponseTime = 0;
+	double concurrentUsers = 0;
+	System(int _thinkTime, vector<Node>& _nodes) {
+		thinkTime = _thinkTime;
+		nodes = _nodes;
 	}
-	~ClosedModel() {}
+	System() {}
+};
 
-	vector<ModelInfo> run() {
-		Qi[0] = 0.0;
-
-		//Iterate over the number(n) of customers
-		vector<ModelInfo> iterations;
-		iterations.reserve(N-1);
-		for (int n = 1; n < N; n++)
+void calculate(vector<vector<System>>& data) {
+	// Calculate
+	for (size_t iThink = 0; iThink < data.size(); iThink++)
+	{
+		vector<System>* iterations = &data[iThink];
+		for (size_t iIter = 1; iIter < iterations->size(); iIter++)
 		{
-			Rprime[n] = Di * (1.0 + Qi[n - 1]);
-			X[n] = n / (Z + Rprime[n]);
-			Qi[n] = X[n] * Rprime[n];
-
-			// open model
-			double Ui = X[n] * Di * 100;
-			double Ri = Di / (1.0 - Ui * 0.01);
-
-			iterations.push_back(ModelInfo(n, Ui, Rprime[n], Ri, X[n], Qi[n]));
+			System* system = &iterations->at(iIter);
+			// calculate Rprime for all nodes
+			for (size_t iNode = 0; iNode < system->nodes.size(); iNode++)
+			{
+				Node* node = &system->nodes[iNode];
+				node->Rprime = node->demand * (1.0 + iterations->at(iIter - 1).nodes[iNode].Q);
+				system->ResponseTime += node->Rprime;
+			}
+			// calculate throughput (Same for all nodes)
+			system->X = iIter / (system->thinkTime + system->ResponseTime);
+			// calculate concurrent users
+			system->concurrentUsers = system->X * system->ResponseTime;
+			// calculate queue length
+			for (size_t iNode = 0; iNode < system->nodes.size(); iNode++)
+			{
+				Node* node = &system->nodes[iNode];
+				node->Q = system->X * node->Rprime;
+			}
 		}
-		return iterations;
 	}
-};
+}
+
+void toFile(string filename, vector<vector<System>>& data) {
+	// write data to file
+	fstream file;
+	file.open(filename, ios::out);
+	if (file.is_open()) {
+		string output = "nrOfPeople\t";
+		for (size_t iThink = 0; iThink < data.size(); iThink++)
+		{
+			int thinkTime = data[iThink][0].thinkTime;
+			string thStr = " (" + to_string(thinkTime) + "s)";
+			output += "Throughput" + thStr + "\t" + "System ResTime" + thStr + "\t" + "Concurrent Users" + thStr + "\t";
+			for (size_t iNode = 0; iNode < data[iThink][0].nodes.size(); iNode++)
+			{
+				string nodeName = data[iThink][0].nodes[iNode].name;
+				output += nodeName + " Queue" + thStr + "\t" + nodeName + " ResTime" + thStr + "\t";
+			}
+		}
+		output += "\n";
+		for (size_t iIter = 0; iIter < data[0].size(); iIter++)
+		{
+			//nr of people
+			output += to_string(iIter) + "\t";
+			for (size_t iThink = 0; iThink < data.size(); iThink++)
+			{
+				System* system = &data[iThink][iIter];
+				// Throughput - Sys resp - conc users
+				output += to_string(system->X) + "\t" + to_string(system->ResponseTime) + "\t" + to_string(system->concurrentUsers) + "\t";
+				// Queue - ResponseTime
+				for (size_t iNode = 0; iNode < system->nodes.size(); iNode++)
+					output += to_string(system->nodes[iNode].Q) + "\t" + to_string(system->nodes[iNode].Rprime) + "\t";
+			}
+			output += "\n";
+		}
+
+		output = replace(output, '.', ',');
+		file << output;
+
+		file.close();
+	}
+}
 
 int main() {
-	// collect data
-	int people = 200;
-	float thinkTime = 10;
-	ClosedModel cpu(0.0394, thinkTime, people);
-	ClosedModel disk1(0.0771, thinkTime, people);
-	ClosedModel disk2(0.1238, thinkTime, people);
-	ClosedModel disk3(0.0804, thinkTime, people);
-	ClosedModel disk4(0.235, thinkTime, people);
-	ClosedModel diskAverage((0.0771 + 0.1238 + 0.0804 + 0.235) / 4, thinkTime, people);
-	cout << 0.0771 + 0.1238 + 0.0804 + 0.235 + 0.0394 << endl;
-	cout << 1.f/(0.0771 + 0.1238 + 0.0804 + 0.235+ 0.0394) << endl;
-	vector<ModelInfo> iter_cpu = cpu.run();
-	vector<ModelInfo> iter_disk1 = disk1.run();
-	vector<ModelInfo> iter_disk2 = disk2.run();
-	vector<ModelInfo> iter_disk3 = disk3.run();
-	vector<ModelInfo> iter_disk4 = disk4.run();
-	vector<ModelInfo> iter_diskAverage = diskAverage.run();
+	vector<Node> systemTest1_nodes = { 
+		Node(0.0394,"Cpu"),
+		Node(0.0771,"Disk1"),
+		Node(0.1238,"Disk2"),
+		Node(0.0804,"Disk3"),
+		Node(0.235,"Disk4") 
+	};
+	double balancedDisksDemand = (0.0771 + 0.1238 + 0.0804 + 0.235) / 4;
+	vector<Node> systemTest2_nodes_balancedDisks = { 
+		Node(0.0394,"Cpu"),
+		Node(balancedDisksDemand,"Disk1"),
+		Node(balancedDisksDemand,"Disk2"),
+		Node(balancedDisksDemand,"Disk3"),
+		Node(balancedDisksDemand,"Disk4") 
+	};
+	vector<Node> systemTest3_nodes_TLS = { 
+		Node(0.137,"Cpu"),
+		Node(balancedDisksDemand,"Disk1"),
+		Node(balancedDisksDemand,"Disk2"),
+		Node(balancedDisksDemand,"Disk3"),
+		Node(balancedDisksDemand,"Disk4") 
+	};
+	double coveringIndexIncrease = 1.0 / 1.55;
+	vector<Node> systemTest4_nodes_CoveringIndex = { 
+		Node(0.0394 * coveringIndexIncrease,"Cpu"),
+		Node(balancedDisksDemand * coveringIndexIncrease,"Disk1"),
+		Node(balancedDisksDemand * coveringIndexIncrease,"Disk2"),
+		Node(balancedDisksDemand * coveringIndexIncrease,"Disk3"),
+		Node(balancedDisksDemand * coveringIndexIncrease,"Disk4") 
+	};
+	double cursorSharingIncrease = 1.0 / 2.0;
+	vector<Node> systemTest5_nodes_CursorSharing = { 
+		Node(0.0394 * cursorSharingIncrease,"Cpu"),
+		Node(balancedDisksDemand * cursorSharingIncrease,"Disk1"),
+		Node(balancedDisksDemand * cursorSharingIncrease,"Disk2"),
+		Node(balancedDisksDemand * cursorSharingIncrease,"Disk3"),
+		Node(balancedDisksDemand * cursorSharingIncrease,"Disk4") 
+	};
 
-	// sort data
-	int iterations = iter_cpu.size();
-	vector<double> system_responseTime;
-	system_responseTime.resize(iterations);
-	vector<double> system_responseTime_balancedDiskLoad;
-	system_responseTime_balancedDiskLoad.resize(iterations);
-	vector<double> line_20Mark;
-	line_20Mark.resize(iterations);
-	vector<double> line_serviceTime3x;
-	line_serviceTime3x.resize(iterations);
-	for (size_t i = 0; i < iterations; i++)
+	vector<int> thinkTimes = { 2, 5, 10 };//in seconds
+
+	// Allocate
+	vector<vector<System>> systemTest1;
+	systemTest1.resize(thinkTimes.size());
+	vector<vector<System>> systemTest2_balancedDisks;
+	systemTest2_balancedDisks.resize(thinkTimes.size());
+	vector<vector<System>> systemTest3_TLS;
+	systemTest3_TLS.resize(thinkTimes.size());
+	vector<vector<System>> systemTest4_CoveringIndex;
+	systemTest4_CoveringIndex.resize(thinkTimes.size());
+	vector<vector<System>> systemTest5_CursorSharing;
+	systemTest5_CursorSharing.resize(thinkTimes.size());
+	for (size_t i = 0; i < thinkTimes.size(); i++)
 	{
-		iter_cpu[i].print();
-
-		system_responseTime[i] = iter_cpu[i].Ri + iter_disk1[i].Ri + iter_disk2[i].Ri + iter_disk3[i].Ri + iter_disk4[i].Ri;
-		system_responseTime_balancedDiskLoad[i] = iter_cpu[i].Ri + iter_diskAverage[i].Ri * 4;
-		line_serviceTime3x[i] = (cpu.Di + disk1.Di + disk2.Di + disk3.Di + disk4.Di)*3;
-		line_20Mark[i] = 20;
-	}
-
-	fstream file;
-
-	//task 1 graph
-	file.open("task1_responseTime_serviceTime3x.txt", ios::out);
-	if (file.is_open()) {
-		file << "Users\tResponseTime\t3 x ServiceTime" << endl;
-		string output = "";
-		for (size_t i = 0; i < system_responseTime.size(); i++)
+		systemTest1[i].resize(200); // nr of users
+		for (size_t iIter = 0; iIter < systemTest1[i].size(); iIter++)
 		{
-			output += to_string(i+1) + "\t" + to_string(system_responseTime[i]) + "\t" + to_string(line_serviceTime3x[i]) + "\n";
+			systemTest1[i][iIter] = System(thinkTimes[i], systemTest1_nodes);
 		}
-		file << replace(output, '.', ',');
-		file.close();
+		systemTest2_balancedDisks[i].resize(300); // nr of users
+		for (size_t iIter = 0; iIter < systemTest2_balancedDisks[i].size(); iIter++)
+		{
+			systemTest2_balancedDisks[i][iIter] = System(thinkTimes[i], systemTest2_nodes_balancedDisks);
+		}
+		systemTest3_TLS[i].resize(300); // nr of users
+		for (size_t iIter = 0; iIter < systemTest3_TLS[i].size(); iIter++)
+		{
+			systemTest3_TLS[i][iIter] = System(thinkTimes[i], systemTest3_nodes_TLS);
+		}
+		systemTest4_CoveringIndex[i].resize(400); // nr of users
+		for (size_t iIter = 0; iIter < systemTest4_CoveringIndex[i].size(); iIter++)
+		{
+			systemTest4_CoveringIndex[i][iIter] = System(thinkTimes[i], systemTest4_nodes_CoveringIndex);
+		}
+		systemTest5_CursorSharing[i].resize(500); // nr of users
+		for (size_t iIter = 0; iIter < systemTest5_CursorSharing[i].size(); iIter++)
+		{
+			systemTest5_CursorSharing[i][iIter] = System(thinkTimes[i], systemTest5_nodes_CursorSharing);
+		}
 	}
 
-	//task 2 graph
-	file.open("task2_responseTime_20sMark.txt", ios::out);
-	if (file.is_open()) {
-		file << "Users\tResponseTime\t20s Mark" << endl;
-		string output = "";
-		for (size_t i = 0; i < system_responseTime.size(); i++)
-		{
-			output += to_string(i + 1) + "\t" + to_string(system_responseTime[i]) + "\t" + to_string(line_20Mark[i]) + "\n";
-		}
-		file << replace(output, '.', ',');
-		file.close();
-	}
-	//task 2 graph disk bottleneck
-	file.open("task2_disksResponseTime.txt", ios::out);
-	if (file.is_open()) {
-		file << "Users\tCpu\tDisk 1\tDisk 2\tDisk 3\tDisk 4" << endl;
-		string output = "";
-		for (size_t i = 0; i < system_responseTime.size(); i++)
-		{
-			output += to_string(i + 1) + "\t" + to_string(iter_cpu[i].Ri) + "\t" + to_string(iter_disk1[i].Ri) + "\t" + to_string(iter_disk2[i].Ri) + "\t" + to_string(iter_disk3[i].Ri) + "\t" + to_string(iter_disk4[i].Ri) +  "\n";
-		}
-		file << replace(output, '.', ',');
-		file.close();
-	}
+	// Calculate
+	calculate(systemTest1);
+	calculate(systemTest2_balancedDisks);
+	calculate(systemTest3_TLS);
+	calculate(systemTest4_CoveringIndex);
+	calculate(systemTest5_CursorSharing);
 
-	//task 3 graph
-	file.open("task3_responseTime_20sMark_balancedDiskLoad.txt", ios::out);
-	if (file.is_open()) {
-		file << "Users\tResponseTime\t20s Mark" << endl;
-		string output = "";
-		for (size_t i = 0; i < system_responseTime.size(); i++)
-		{
-			output += to_string(i + 1) + "\t" + to_string(system_responseTime_balancedDiskLoad[i]) + "\t" + to_string(line_20Mark[i]) + "\n";
-		}
-		file << replace(output, '.', ',');
-		file.close();
-	}
+	toFile("data_test1.txt", systemTest1);
+	toFile("data_test2_balancedDisks.txt", systemTest2_balancedDisks);
+	toFile("data_test3_TLS.txt", systemTest3_TLS);
+	toFile("data_test4_CoveringIndex.txt", systemTest4_CoveringIndex);
+	toFile("data_test5_CursorSharing.txt", systemTest5_CursorSharing);
+
 
 	return 0;
 }
